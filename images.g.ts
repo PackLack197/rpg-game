5a46163b7ea59b170ddaa0adbffb392d8bd2dce3
PackLack197 <108018568+PackLack197@users.noreply.github.com>
2022-06-23 10:46:29 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -22,12 +22,119 @@ f 6 1 1 1 1 1 6 6 6 6 6 c f . .
 . f 6 1 1 1 1 1 1 6 6 6 f . . . 
 . . c c c c c c c c c f . . . . 
 `;
-            case "image2":
-            case "sword":return img`
-1 1 1 1 
-1 1 1 1 
-1 1 1 1 
-1 1 1 1 
+            case "image3":
+            case "arrowUp":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . f f e f f . . . . . . 
+. . . . f f . e . f f . . . . . 
+. . . . . . . e . . . . . . . . 
+. . . . . . . e . . . . . . . . 
+. . . . . . . e . . . . . . . . 
+. . . . . . . e . . . . . . . . 
+. . . . . . . e . . . . . . . . 
+. . . . . . . e . . . . . . . . 
+. . . . . . . e . . . . . . . . 
+. . . . . 1 1 1 1 1 . . . . . . 
+. . . . . 1 1 1 1 1 . . . . . . 
+. . . . . 1 1 1 1 1 . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":
+            case "arrowLeft":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f . . . . . . . . . . . 
+. . . f f . . . . . . . 1 1 1 . 
+. . f f . . . . . . . . 1 1 1 . 
+. . f e e e e e e e e e 1 1 1 . 
+. . f f . . . . . . . . 1 1 1 . 
+. . . f f . . . . . . . 1 1 1 . 
+. . . . f . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "arrowDown":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 1 1 1 1 1 . . . . . 
+. . . . . . 1 1 1 1 1 . . . . . 
+. . . . . . 1 1 1 1 1 . . . . . 
+. . . . . . . . e . . . . . . . 
+. . . . . . . . e . . . . . . . 
+. . . . . . . . e . . . . . . . 
+. . . . . . . . e . . . . . . . 
+. . . . . . . . e . . . . . . . 
+. . . . . . . . e . . . . . . . 
+. . . . . . . . e . . . . . . . 
+. . . . . f f . e . f f . . . . 
+. . . . . . f f e f f . . . . . 
+. . . . . . . f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "arrowRight":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . f . . . . 
+. 1 1 1 . . . . . . . f f . . . 
+. 1 1 1 . . . . . . . . f f . . 
+. 1 1 1 e e e e e e e e e f . . 
+. 1 1 1 . . . . . . . . f f . . 
+. 1 1 1 . . . . . . . f f . . . 
+. . . . . . . . . . . f . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "2=y=8y)5*$BbA[tn`":
+            case "mushroom":return img`
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 3 3 3 3 3 1 1 b b c c . 
+. . b 1 1 3 3 3 3 3 1 1 3 3 c c 
+. . b 1 1 3 3 3 3 3 3 3 3 3 b c 
+. . c 3 3 3 3 3 3 3 c c c b b f 
+. c 3 3 3 3 3 b b b b c c c b f 
+c 3 3 3 3 b b d d d d d c c b f 
+c 3 3 c b d d d d d d c d c c . 
+f 3 c c c d d c d d d c d b c . 
+f b c c c d d d c d d d d d f . 
+f b c c c d d d d d b b b d f . 
+f f b b c b d d d d d d d c . . 
+. f f f f b c c d d d d f f . . 
+. . f b d d b c c f f b b f f . 
+. . f d d d b . . f f b b b f . 
+`;
+            case "i_U,7Gp97d`yJ3^r[q[T":
+            case "empty":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
 `;
         }
         return null;
@@ -35,8 +142,8 @@ f 6 1 1 1 1 1 6 6 6 6 6 c f . .
 
     helpers._registerFactory("animation", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "myAnim":
-            case "anim1":return [img`
+            case "mushroomWalkLeft":
+            case "anim2":return [img`
 . . . . . . . . b b b b . . . . 
 . . . . b b b b 3 3 3 3 b . . . 
 . c c b b 1 1 3 3 3 3 3 b b . . 
@@ -104,6 +211,216 @@ f b c c d d d d d b b 3 3 3 3 c
 . . f f d d d d c c b f f f f . 
 . f f b b f f c c b d d b f . . 
 . f b b b f f . . b d d d f . . 
+`];
+            case "mushroomWalkRight":
+            case "anim1":return [img`
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 3 3 3 3 3 1 1 b b c c . 
+. . b 1 1 3 3 3 3 3 1 1 3 b c c 
+. . b 1 1 3 3 3 3 3 3 3 3 3 b c 
+. . c 3 3 3 3 3 b c c c c b b f 
+. c 3 3 3 3 b b d d d c c c b f 
+c b 3 3 b b d d d d d d b c b f 
+c 3 3 c b d d d d d d c d b c . 
+f 3 c c c d d c d d d c d d c . 
+f b c c c d d d c d d d d d f . 
+f b c c c f f b d d b b b d f . 
+f f b b f b d d b d d d d c . . 
+. f f f f d d b b d d d c . . . 
+. . . . b b b b f b b f f . . . 
+. . . . . . . f f b b b f . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 3 3 3 3 3 1 1 b b c c . 
+. . b 1 1 3 3 3 3 1 1 1 3 c c c 
+. . b 1 1 3 3 3 3 3 3 3 3 3 b c 
+. . c 1 1 3 3 b b c c c c b b f 
+. c c 3 3 b b d d d d b c c b f 
+c b 3 3 b b d d d d d d d c b f 
+c 3 3 b b d d d d d d c d d c . 
+f 3 3 c b d d c d d d c d d c . 
+f b c c c d d d c d d d d d f . 
+f b c c c d d f f b b b b d f . 
+f f b b c c f b d d b d d c . . 
+. f f f c c f d d b b d c . . . 
+. . . . . . b b b b f c . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 3 3 3 3 3 3 1 1 b c c . 
+. . b 3 3 3 3 3 3 1 1 1 3 c c c 
+. . b 1 1 3 3 3 3 3 3 3 3 3 b c 
+. . c 1 1 3 3 3 b c c c c b b f 
+. c c 3 3 3 b b d d d c c c b f 
+c b 3 3 b b d d d d d d b c b f 
+c 3 3 c b d d d d d d d d b c . 
+f 3 c c c d d d d d d c c d c . 
+f b c c c d d c c d d d d d f . 
+f b c c c d d d d d b b b d f . 
+f f b b c f f b d d d d d c . . 
+. f f f f d d b b d d d b f . . 
+. . . . f d d d b c c f f f . . 
+`, img`
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 3 3 3 3 3 1 1 b b c c . 
+. . b 1 1 3 3 3 3 3 1 1 3 3 c c 
+. . b 1 1 3 3 3 3 3 3 3 3 3 b c 
+. . c 3 3 3 3 3 3 3 c c c b b f 
+. c 3 3 3 3 3 b b b b c c c b f 
+c 3 3 3 3 b b d d d d d c c b f 
+c 3 3 c b d d d d d d c d c c . 
+f 3 c c c d d c d d d c d b c . 
+f b c c c d d d c d d d d d f . 
+f b c c c d d d d d b b b d f . 
+f f b b c b d d d d d d d c . . 
+. f f f f b c c d d d d f f . . 
+. . f b d d b c c f f b b f f . 
+. . f d d d b . . f f b b b f . 
+`];
+            case "mushroomBackWalk":
+            case "anim3":return [img`
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 3 3 3 3 3 1 1 b b c c . 
+. . b 1 1 3 3 3 3 3 1 1 b b c c 
+. . b 1 1 3 3 3 3 3 b b b b b c 
+. . c 3 3 3 3 3 3 3 b b b b b f 
+. c b 3 3 3 3 3 3 b b b b b b f 
+c b b b 3 3 3 b b b b b b b b f 
+c b b b b b b b b b b b b b c . 
+f b b b b 1 1 b b b b b b c c . 
+f b b b b 1 1 b b b b b c d f . 
+f b b b b b b b b b c c d d f . 
+f f b b b b b b c c d d d c . . 
+. f f f c c c c d d d d c . . . 
+. . . . b b b b f c c c f . . . 
+. . . . . . . f f d d d f . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 1 3 3 3 3 1 1 1 b c c . 
+. . b 1 1 3 3 3 3 3 1 1 b b b c 
+. . b 1 1 3 3 3 3 3 b b b b b c 
+. . c 3 3 3 3 3 3 b b b b b b f 
+. c b b 3 3 3 3 3 b b b b b b f 
+c b b b 3 3 3 b b b b b b b b f 
+c b b b b 1 1 b b b b b b b c . 
+f b b b b 1 1 b b b b b b c c . 
+f b b b b 3 3 b b b b c c d f . 
+f c b b b b b b b c c d d d f . 
+f f c b b b b c c d d d d c . . 
+. f f f c c c c d d d d c . . . 
+. . . . . . b b f c c c . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 3 3 3 3 3 1 1 b b c c . 
+. . b 1 1 3 3 3 3 3 1 1 b b c c 
+. . b 1 1 3 3 3 3 3 b b b b b c 
+. . c 3 3 3 3 3 3 3 b b b b b f 
+. c b 3 3 3 3 3 3 b b b b b b f 
+c b b b 3 3 3 b b b b b b b b f 
+c b b b b b b b b b b b b b c . 
+f b b b b 1 1 b b b b b b c c . 
+f b b b b 1 1 b b b b b c d f . 
+f b b b b b b b b b c c d d f . 
+f f b b b b b b c c d d d c . . 
+. f f f c c c c d d d d c f . . 
+. . . . f b b b f c c c f f . . 
+`, img`
+. . . . b b b b . . . . . . . . 
+. . . b 3 3 3 3 b b b b . . . . 
+. . b b 3 3 3 3 3 1 1 b c c c . 
+. . b 1 3 3 3 3 3 1 1 1 b c c c 
+. . b 1 1 3 3 3 3 3 b b b b c c 
+. . c 1 1 3 3 3 3 3 b b b b b f 
+. c c 3 3 3 3 3 3 3 b b b b b f 
+c c b 3 3 3 3 3 b b b b b b b f 
+c b b b b b b b b b b b b b c . 
+f b b b b 3 3 b b b b b b b c . 
+f b b b b 1 1 b b b b b b c f . 
+f b b b b 1 1 b b b b c c d f . 
+f f b b b b b b b c c d d c . . 
+. f f f c c c c c d d d c f . . 
+. . f f b b f c f c c c b f f . 
+. . f b b b f . . f b d d d f . 
+`];
+            case "myAnim":
+            case "anim4":return [img`
+. . . . . . . . b b b b . . . . 
+. . . . b b b b 3 3 3 3 b . . . 
+. c c b b 1 1 3 3 3 3 3 b b . . 
+c c b b 1 1 3 3 3 3 3 1 1 b . . 
+c b b b b b 3 3 3 3 3 1 1 b . . 
+f b b b b b 3 3 3 3 3 3 3 c . . 
+f b b b b b b 3 3 3 3 3 3 b c . 
+f b b b b b b b b 3 3 3 b b b c 
+. c b b b b b b b b b b b b b c 
+. c c b b b b b b 1 1 b b b b f 
+. f d c b b b b b 1 1 b b b b f 
+. f d d c c b b b b b b b b b f 
+. . c d d d c c b b b b b b f f 
+. . . c d d d d c c c c f f f . 
+. . . f c c c f b b b b . . . . 
+. . . f d d d f f . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . b b b b . . . . 
+. . . . b b b b 3 3 3 3 b . . . 
+. c c b 1 1 1 3 3 3 3 1 b b . . 
+c b b b 1 1 3 3 3 3 3 1 1 b . . 
+c b b b b b 3 3 3 3 3 1 1 b . . 
+f b b b b b b 3 3 3 3 3 3 c . . 
+f b b b b b b 3 3 3 3 3 b b c . 
+f b b b b b b b b 3 3 3 b b b c 
+. c b b b b b b b 1 1 b b b b c 
+. c c b b b b b b 1 1 b b b b f 
+. f d c c b b b b 3 3 b b b b f 
+. f d d d c c b b b b b b b c f 
+. . c d d d d c c b b b b c f f 
+. . . c d d d d c c c c f f f . 
+. . . . c c c f b b . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . b b b b . . . . 
+. . . . b b b b 3 3 3 3 b . . . 
+. c c b b 1 1 3 3 3 3 3 b b . . 
+c c b b 1 1 3 3 3 3 3 1 1 b . . 
+c b b b b b 3 3 3 3 3 1 1 b . . 
+f b b b b b 3 3 3 3 3 3 3 c . . 
+f b b b b b b 3 3 3 3 3 3 b c . 
+f b b b b b b b b 3 3 3 b b b c 
+. c b b b b b b b b b b b b b c 
+. c c b b b b b b 1 1 b b b b f 
+. f d c b b b b b 1 1 b b b b f 
+. f d d c c b b b b b b b b b f 
+. . c d d d c c b b b b b b f f 
+. . f c d d d d c c c c f f f . 
+. . f f c c c f b b b f . . . . 
+`, img`
+. . . . . . . . b b b b . . . . 
+. . . . b b b b 3 3 3 3 b . . . 
+. c c c b 1 1 3 3 3 3 3 b b . . 
+c c c b 1 1 1 3 3 3 3 3 1 b . . 
+c c b b b b 3 3 3 3 3 1 1 b . . 
+f b b b b b 3 3 3 3 3 1 1 c . . 
+f b b b b b 3 3 3 3 3 3 3 c c . 
+f b b b b b b b 3 3 3 3 3 b c c 
+. c b b b b b b b b b b b b b c 
+. c b b b b b b b 3 3 b b b b f 
+. f c b b b b b b 1 1 b b b b f 
+. f d c c b b b b 1 1 b b b b f 
+. . c d d c c b b b b b b b f f 
+. . f c d d d c c c c c f f f . 
+. f f b c c c f c f b b f f . . 
+. f d d d b f . . f b b b f . . 
 `];
         }
         return null;
--- main.ts
+++ main.ts
@@ -1,35 +1,110 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    DownPressed = false
+    LeftPressed = false
+    RightPressed = false
+    UpPressed = true
+    animation.runImageAnimation(
+    playerMushroom,
+    assets.animation`mushroomBackWalk`,
+    200,
+    true
+    )
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (LeftPressed) {
+        projectile = sprites.createProjectileFromSprite(assets.image`arrowLeft`, playerMushroom, -150, 0)
+    } else if (DownPressed) {
+        projectile = sprites.createProjectileFromSprite(assets.image`arrowDown`, playerMushroom, 0, 150)
+    } else if (RightPressed) {
+        projectile = sprites.createProjectileFromSprite(assets.image`arrowRight`, playerMushroom, 150, 0)
+    } else if (UpPressed) {
+        projectile = sprites.createProjectileFromSprite(assets.image`arrowUp`, playerMushroom, 0, -150)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Level2Teleporter`, function (sprite, location) {
+    if (level1Done) {
+        tiles.setCurrentTilemap(tilemap`level2`)
+    } else {
+        game.splash("Whoa, whoa, whoa! You haven't finished level 1!")
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    DownPressed = false
+    LeftPressed = true
+    RightPressed = false
+    UpPressed = false
+    animation.runImageAnimation(
+    playerMushroom,
+    assets.animation`mushroomWalkLeft`,
+    200,
+    true
+    )
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    if (game.askForNumber("What is 2+2?", 1) == 4) {
+        game.showLongText("Correct! Go to level 2!", DialogLayout.Bottom)
+        tiles.setCurrentTilemap(tilemap`teleportBase`)
+        level1Done = true
+    } else {
+        game.showLongText("That's wrong. Try again!", DialogLayout.Bottom)
+    }
+    pause(2000)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    DownPressed = false
+    LeftPressed = false
+    RightPressed = true
+    UpPressed = false
+    animation.runImageAnimation(
+    playerMushroom,
+    assets.animation`mushroomWalkRight`,
+    100,
+    true
+    )
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Level1Teleporter`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level1`)
+    snake1level1 = sprites.create(assets.image`enemySnake`, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(snake1level1, sprites.castle.tileDarkGrass2)
+    snake1level1.follow(playerMushroom, 50)
+    snake2level1 = sprites.create(assets.image`enemySnake`, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(snake2level1, sprites.castle.tileDarkGrass2)
+    snake2level1.follow(playerMushroom, 50)
+})
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
-    tiles.setCurrentTilemap(tilemap`level4`)
-    mySprite2 = sprites.create(assets.image`enemySnake`, SpriteKind.Enemy)
-    tiles.placeOnRandomTile(mySprite2, sprites.castle.tileDarkGrass2)
-    mySprite2.follow(mySprite)
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    DownPressed = true
+    LeftPressed = false
+    RightPressed = false
+    UpPressed = false
+    animation.runImageAnimation(
+    playerMushroom,
+    assets.animation`myAnim`,
+    200,
+    true
+    )
 })
-let mySprite2: Sprite = null
-let mySprite: Sprite = null
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.ashes, 500)
+    sprite.destroy()
+})
+let snake2level1: Sprite = null
+let snake1level1: Sprite = null
+let level1Done = false
+let projectile: Sprite = null
+let UpPressed = false
+let RightPressed = false
+let LeftPressed = false
+let DownPressed = false
+let playerMushroom: Sprite = null
 scene.setBackgroundColor(9)
-tiles.setCurrentTilemap(tilemap`level3`)
-mySprite = sprites.create(img`
-    . . . . b b b b . . . . . . . . 
-    . . . b 3 3 3 3 b b b b . . . . 
-    . . b b 3 3 3 3 3 1 1 b b c c . 
-    . . b 1 1 3 3 3 3 3 1 1 3 3 c c 
-    . . b 1 1 3 3 3 3 3 3 3 3 3 b c 
-    . . c 3 3 3 3 3 3 3 c c c b b f 
-    . c 3 3 3 3 3 b b b b c c c b f 
-    c 3 3 3 3 b b d d d d d c c b f 
-    c 3 3 c b d d d d d d c d c c . 
-    f 3 c c c d d c d d d c d b c . 
-    f b c c c d d d c d d d d d f . 
-    f b c c c d d d d d b b b d f . 
-    f f b b c b d d d d d d d c . . 
-    . f f f f b c c d d d d f f . . 
-    . . f b d d b c c f f b b f f . 
-    . . f d d d b . . f f b b b f . 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite, 100, 100)
-scene.cameraFollowSprite(mySprite)
-mySprite.setStayInScreen(true)
+tiles.setCurrentTilemap(tilemap`teleportBase`)
+playerMushroom = sprites.create(assets.image`mushroom`, SpriteKind.Player)
+controller.moveSprite(playerMushroom, 100, 100)
+scene.cameraFollowSprite(playerMushroom)
+playerMushroom.setStayInScreen(true)
 info.setLife(1000)
+game.showLongText("You know you can't stall any longer. Your one goal is to DESTROY THE SNAKES!", DialogLayout.Bottom)
+game.showLongText("Step on the 1 to go to the first level!", DialogLayout.Bottom)
